805f69f5e4a3f98d7ea59ca4c28fb6007bca85e3
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-09-06 21:02:04 -0300

    cleanup

---
+++ 3/3.js
@@ -0,0 +1,31 @@
+// #region Setup
+const API_URL = 'https://starwars.egghead.training/'
+const output = document.getElementById('output')
+const spinner = document.getElementById('spinner')
+
+const getFilmTitles = (films) => films
+    .sort((a, b) => a.episode_id - b.episode_id)
+    .map(film => `${film.episode_id}. ${film.title}`)
+    .join('\n')
+// #endregion
+
+fetch(`${API_URL}films`).then(
+    // onFulfilled
+    response => {
+        if (!response.ok) {
+            throw Error("Unsuccessful response")
+        }
+
+        return response.json()
+            .then(films => output.innerText = getFilmTitles(films))
+    })
+    .catch(error => {
+        // onRejected
+        console.warn(error)
+        output.innerText = ':('    
+    })
+    .finally(() => {
+        spinner.remove()
+    })
+
+
